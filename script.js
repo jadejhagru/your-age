5d9ae53b7b896ba5cc5ff75c131cf2e0278c1fe4
Jade Jhagru <j.jhagru@hotmail.com>
2021-03-28 12:17:05 -0400

    new element

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function ageInDays() {
 var birthYear = prompt("What year were you born good friend?");
 var finalAge = (2021 - birthYear) * 365;
 var h1 = document.createElement("h1");
-var textAnswer = document.createTextNode("You are  " + finalAge + "days old");
+var textAnswer = document.textContent("You are  " + finalAge + "days old");
 h1.setAttribute("id", "ageInDays");
 h1.appendChild(textAnswer);
 document.getElementById("flex-box-result").appendChild(h1);
