865d06d78da7e4911beafa0eb4ed189abde53348
Jade Jhagru <j.jhagru@hotmail.com>
2021-03-28 13:00:38 -0400

    css styling

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ h1.setAttribute("id", "ageInDays");
 h1.appendChild(textAnswer);
 document.getElementById("flex-box-result").appendChild(h1);
 }
+
+function reset() {
+    document.getElementById("ageInDays").remove();
+}
